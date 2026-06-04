f86528eddf7d20069cc52de6327fcad3c110bcbc
Roman Singh <contact@seamless.club>
2026-06-04 12:05:34 +0800

    🔧 Add GH Pages deploy mode (base=/universe-seamless/)

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: mode === 'github' ? '/universe-seamless/' : '/',
   server: { host: "0.0.0.0", port: 5173 },
   preview: {
     host: "0.0.0.0",
     port: 5183,
     allowedHosts: ["universe.seamless.club", "universe.158-220-125-28.nip.io"],
   },
-});
+}));
